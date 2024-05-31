5639e7c884f881e34811e284d5fada6daa27eb2c
Poggu <poggu@seznam.cz>
2024-05-31 07:45:02 +0200

    add missing defines

--- xmake.lua
+++ xmake.lua
@@ -85,6 +85,8 @@ target("CS2ServerGUI")
     add_defines({
         "COMPILER_MSVC",
         "COMPILER_MSVC64",
+        "PLATFORM_64BITS",
+        "X64BITS",
         "WIN32",
         "WINDOWS",
         "CRT_SECURE_NO_WARNINGS",
